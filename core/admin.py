562f8f5a087c375fbd9789c1c98f8f7a854616ed
Jcasa08 <joecasamassa@yahoo.co.uk>
2026-01-07 14:21:44 +0000

    register unit model with dj-summernote

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Faction, Unit
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Unit)
+class UnitAdmin(SummernoteModelAdmin):
+    summernote_fields = ('name',)
+    list_display = ('name', 'faction', 'role', 'points', 'min_size', 'max_size')
+    list_filter = ('faction', 'role')
+    search_fields = ('name',)
+
 
 # Register your models here.
 admin.site.register(Faction)
-admin.site.register(Unit)
