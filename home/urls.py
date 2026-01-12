7a7b98400ddd45246b17b44496673e860afa50ca
Jcasa08 <joecasamassa@yahoo.co.uk>
2026-01-12 16:03:30 +0000

    add dynamic routing for faction buttons

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("world/", views.WorldPageView.as_view(), name="world"),
     path("factions/", views.FactionPageView.as_view(), name="factions"),
+    path("factions/<str:faction_name>/",
+         views.FactionDetailView.as_view(), name="faction_detail"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from core.models import Faction
 
 # Create your views here.
+
 
 class HomePageView(TemplateView):
     template_name = "home/home.html"
@@ -9,5 +12,16 @@ class HomePageView(TemplateView):
 class WorldPageView(TemplateView):
     template_name = "home/world.html"
 
+
 class FactionPageView(TemplateView):
     template_name = "home/factions.html"
+
+
+class FactionDetailView(TemplateView):
+    template_name = "home/faction_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        faction_name = kwargs.get('faction_name')
+        context['faction'] = get_object_or_404(Faction, name=faction_name)
+        return context
