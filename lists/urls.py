d450b618203706d55b4140e953dc7ae51f324abd
Jcasa08 <joecasamassa@yahoo.co.uk>
2026-01-13 17:19:04 +0000

    add armylist delete view

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import ArmyListListView, ArmyListDetailView, ArmyListCreateView
+from .views import (
+    ArmyListListView,
+    ArmyListDetailView,
+    ArmyListCreateView,
+    ArmyListUpdateView,
+    ArmyListDeleteView,
+)
 
 urlpatterns = [
     path('', ArmyListListView.as_view(), name='armylist_list'),
     path('create/', ArmyListCreateView.as_view(), name='armylist_create'),
     path('<int:pk>/', ArmyListDetailView.as_view(), name='armylist_detail'),
+    path('<int:pk>/edit/', ArmyListUpdateView.as_view(), name='armylist_update'),
+    path('<int:pk>/delete/', ArmyListDeleteView.as_view(), name='armylist_delete'),
 ]
