8eccb927b1b8396d38563e60506609f7e86f2385
Jcasa08 <joecasamassa@yahoo.co.uk>
2026-01-13 17:11:04 +0000

    create army list form view

--- lists/urls.py
+++ lists/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArmyListListView, ArmyListDetailView
+from .views import ArmyListListView, ArmyListDetailView, ArmyListCreateView
 
 urlpatterns = [
     path('', ArmyListListView.as_view(), name='armylist_list'),
+    path('create/', ArmyListCreateView.as_view(), name='armylist_create'),
     path('<int:pk>/', ArmyListDetailView.as_view(), name='armylist_detail'),
 ]
