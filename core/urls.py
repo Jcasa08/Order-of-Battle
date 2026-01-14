4a07910782deff9682b16a92c9016686c2863b44
Jcasa08 <joecasamassa@yahoo.co.uk>
2026-01-14 13:07:31 +0000

    removed index path in core urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('api/unit/<int:unit_id>/', views.unit_details, name='unit_details'),
 ]
