3e8cb1c15a1c10812c40789e1c665de6710b01ef
Jcasa08 <joecasamassa@yahoo.co.uk>
2026-01-23 14:40:48 +0000

    add automated tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+# core/tests.py
+from django.test import TestCase, Client
+from django.urls import reverse
+from core.models import Faction, Unit
 
-# Create your tests here.
+
+class UnitDetailsAPITests(TestCase):
+    def setUp(self):
+        faction = Faction.objects.create(name="Orc Tribes")
+        self.unit = Unit.objects.create(
+            name="Warband",
+            faction=faction,
+            role="core",
+            points=120,
+            min_size=5,
+            max_size=20,
+        )
+        self.client = Client()
+
+    def test_unit_details_returns_json(self):
+        resp = self.client.get(reverse("unit_details", args=[self.unit.id]))
+        self.assertEqual(resp.status_code, 200)
+        data = resp.json()
+        self.assertEqual(data["min_size"], self.unit.min_size)
+        self.assertEqual(data["max_size"], self.unit.max_size)
+        self.assertEqual(data["points"], self.unit.points)
+
+    def test_unit_details_missing_returns_404(self):
+        resp = self.client.get(reverse("unit_details", args=[9999]))
+        self.assertEqual(resp.status_code, 404)
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+# home/tests.py
+from django.test import TestCase, Client
+from django.urls import reverse
+from core.models import Faction
 
-# Create your tests here.
+class HomeViewsTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.faction = Faction.objects.create(name="Duchies of Men")
+
+    def test_home_page_renders(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "home/home.html")
+
+    def test_world_page_renders(self):
+        resp = self.client.get(reverse("world"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "home/world.html")
+
+    def test_factions_page_renders(self):
+        resp = self.client.get(reverse("factions"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "home/factions.html")
+
+    def test_faction_detail_existing(self):
+        resp = self.client.get(reverse("faction_detail", args=[self.faction.name]))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.context["faction"].name, self.faction.name)
+
+    def test_faction_detail_missing_returns_404(self):
+        resp = self.client.get(reverse("faction_detail", args=["Nonexistent"]))
+        self.assertEqual(resp.status_code, 404)
